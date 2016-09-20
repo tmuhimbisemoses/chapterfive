9a0ca7024f5032b3a907fe2682c290934d5f9611
tmuhimbisemoses <tumuhimbise.moses@gmail.com>
2016-09-20 17:15:20 +0300

    Answer to question two chapter three

---
+++ q2c3.rb
@@ -0,0 +1,11 @@
+puts "Enter any number below 10:"
+x = gets
+x= gets.to_i
+puts "Enter any number below 10:"
+y = gets
+y= gets.to_i
+puts "Enter any number below 10:"
+z = gets
+z= gets.to_i
+eqn1=x*y/z+y-x
+puts eqn1
